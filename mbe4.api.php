2c7e1f9c41ca506cf89dd9d8fc100c7c3c637fde
root <root@dev.justmakeit.biz>
2012-09-30 03:32:27 +0400

    mbe4-Klasse integriert

--- mbe4.api.php
+++ mbe4.api.php
@@ -86,9 +86,68 @@ class mbe4 {
   return $data;
   }
   
-  function validate_transaction($data) {
-    
+/*	validate_transaction($mbe4_params)
+*	Validierung der Zahlung. Dafür werden die von mbe4 übergebenen Parameter 
+*	plus das mbe4-Password hintereinander gehängt und eine MD5-Summe aus dem String erstellt.
+*	params:
+*		$mbe4_params: Key-Value-Array aus dem per GET übergebenen Parametern
+*	return:
+*		Response-Code, 0 bedeutet erfolgreiche Zahlung
+*
+*/
+  function validate_transaction($mbe4_params, $mbe4_password) {
+      // check hash Signierung
+      $hashbase=
+	  $mbe4_password . 
+	  $mbe4_params["transactionid"] . 
+	  $mbe4_params["clienttransactionid"] . 
+	  $mbe4_params["responsecode"] . 
+	  $mbe4_params["description"] . 
+	  $mbe4_params["subscriberid"] . 
+	  $mbe4_params["operatorid"] . 
+	  $mbe4_params["timestamp"];
+      if(md5($hashbase)!=$mbe4_params["hash"]){
+	  return 999; // Parameter nicht korrekt oder manipuliert!
+      }
+      return $mbe4_params["responsecode"]; // Transaktionscode zurückgeben
   }
   
+  function is_valid_responsecode($responsecode) {
+    if($responsecode==0)
+	return TRUE;
+    else
+	return FALSE;
+  }
+  
+  function get_responsemsg_by_responsecode($responsecode){
+    switch($responsecode){
+	case 0: return('OK');
+	case 1: return('NOT FINAL – request was processed successfully but the answer is not final. (e.g. a TAN was sent to the subscriber and tan received must be called)');
+	case 2: return('authorization failed');
+	case 3: return('capture failed');
+	case 4: return('terminate failed');
+	case 5: return('refund failed');
+	case 6: return('prepair failed');
+	case 7: return('transaction failed');
+	case 8: return('subscription terminate failed');
+	case 101: return('invalid parameter');
+	case 109: return('transaction in wrong status');
+	case 110: return('wrong PIN');
+	case 111: return('too many PIN attempts - transaction closed');
+	case 112: return('subscriber aborted transaction');
+	case 113: return('no route to operator');
+	case 121: return('subscriberid unascertainable');
+	case 126: return('sending TAN SMS failed');
+	case 150: return('subscriptionid unknown');
+	case 151: return('subscriptionid not unique');
+	case 152: return('subscription terminated');
+	case 200: return('internal server error');
+	case 999: return('hash cant be verified');
+	case 201: return('system currently unavailable');
+	default: return(900);
+    }
+  }
 }
+  
+
 ?>
